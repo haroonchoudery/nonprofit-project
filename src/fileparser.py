e27f5c3d99804db41b67f46496b397e5a0459266
Jerry Song <songlingyi@gmail.com>
2016-11-28 00:14:22 +0000

    add more code

---
+++ src/fileparser.py
@@ -0,0 +1,65 @@
+import ijson
+import urllib2
+from lxml import etree
+from organization import Organization
+
+# The number of objects to process in the source index file
+RANGE = 20
+
+# Keys in the json index file
+ORGANIZATION_NAME = 'OrganizationName'
+ELECTRONIC_ID = 'EIN'
+URL = 'URL'
+FORM_TYPE = 'FormType'
+
+# Paths in the tax form xml file
+ORGANIZATION_TYPE = "//*[re:test(local-name(), '^Organization.*')]"
+CURRENT_YEAR_REVENUE = './/{http://www.irs.gov/efile}CYTotalRevenueAmt'
+PRIOR_YEAR_REVENUE = './/{http://www.irs.gov/efile}PYTotalRevenueAmt'
+
+def parse_json_index(json_file, prefix):
+    """This function will parse a given index file in json format."""
+
+    index = open(json_file)
+    objects = ijson.items(index, prefix)
+
+    i = 0
+    objects_990 = (obj for obj in objects if obj[FORM_TYPE] == '990')
+    for obj in objects_990:
+        if i == RANGE:
+            break
+        else:
+            i += 1
+            organization_type, current_year_revenue, prior_year_revenue = parse_xml_form(obj[URL])
+            organization = Organization(obj[ELECTRONIC_ID],
+                                        obj[ORGANIZATION_NAME],
+                                        organization_type,
+                                        current_year_revenue,
+                                        prior_year_revenue)
+            yield organization
+
+def parse_xml_form(url):
+    tree = etree.ElementTree(file=urllib2.urlopen(url))
+    root=tree.getroot()
+
+    current_year_revenue = get_current_year_revenue(root)
+    prior_year_revenue = get_prior_year_revenue(root)
+    organization_type = get_organization_type(root)
+    return organization_type, current_year_revenue, prior_year_revenue
+
+def get_current_year_revenue(root):
+    current_year_revenue = root.find(CURRENT_YEAR_REVENUE)
+    return current_year_revenue.text if current_year_revenue is not None else None
+
+def get_prior_year_revenue(root):
+    prior_year_revenue = root.find(PRIOR_YEAR_REVENUE)
+    return prior_year_revenue.text if prior_year_revenue is not None else None
+
+def get_organization_type(root):
+    organization_tag = root.xpath(ORGANIZATION_TYPE,
+                                  namespaces={'re': "http://exslt.org/regular-expressions"})[0].tag
+    if organization_tag is None:
+        return None
+    else:
+        organization_type = organization_tag.split('Organization')[1]
+        return organization_type
--- src/organization.py
+++ src/organization.py
@@ -7,15 +7,27 @@ class Organization():
         self.electronic_id = int(electronic_id)
         self.organization_name = organization_name
         self.organization_type = organization_type
-        self.current_year_revenue = int(current_year_revenue)
-        self.prior_year_revenue = int(prior_year_revenue)
-        self.annual_revenue_growth = \
-            self.compute_annual_revenue_growth(current_year_revenue,
-                                               prior_year_revenue)
+        self.set_current_year_revenue(current_year_revenue)
+        self.set_prior_year_revenue(prior_year_revenue)
+        self.set_annual_revenue_growth(current_year_revenue, prior_year_revenue)
 
-    def compute_annual_revenue_growth(self,
-                                      current_year_revenue,
-                                      prior_year_revenue):
-        growth = float(current_year_revenue) / float(prior_year_revenue) - 1
-        growth =round(growth, 2)
-        return growth
+    def set_current_year_revenue(self, current_year_revenue):
+        if current_year_revenue is not None:
+            self.current_year_revenue = int(current_year_revenue)
+        else:
+            self.current_year_revenue = None
+
+    def set_prior_year_revenue(self, prior_year_revenue):
+        if prior_year_revenue is not None:
+            self.prior_year_revenue = int(prior_year_revenue)
+        else:
+            self.prior_year_revenue = None
+
+    def set_annual_revenue_growth(self, current_year_revenue, prior_year_revenue):
+        if current_year_revenue is None or \
+           prior_year_revenue is None or \
+           prior_year_revenue is 0:
+           self.annual_revenue_growth = None
+        else:
+           growth = float(current_year_revenue) / float(prior_year_revenue) - 1
+           self.annual_revenue_growth =round(growth, 2)
