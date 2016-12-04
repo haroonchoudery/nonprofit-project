eb1d4095211ec7677972aefb85e45d0a484000a5
Jerry Song <songlingyi@gmail.com>
2016-12-04 05:29:42 +0000

    add a status call api

--- src/webapp.py
+++ src/webapp.py
@@ -3,7 +3,7 @@ from db.dbclient import DBClient
 
 mysql_client = DBClient()
 
-@route('/growth/<key>')
+@route('/growth/<key>', method = 'GET')
 def get_revenue_growth(key):
     revenue_growth = mysql_client.query_revenue_growth(key)
     if revenue_growth is None:
@@ -12,7 +12,7 @@ def get_revenue_growth(key):
         return 'The annual growth of the organization with electronic id or name %s is %s\n' \
                % (key, revenue_growth)
 
-@route('/ranking/<organization_type>/<limit>')
+@route('/ranking/<organization_type>/<limit>', method = 'GET')
 def get_revenue_growth_ranking(organization_type, limit):
     revenue_growth_ranking = mysql_client.query_by_type(organization_type, int(limit))
     if len(revenue_growth_ranking) == 0:
@@ -20,5 +20,9 @@ def get_revenue_growth_ranking(organization_type, limit):
     else:
         return 'The annual growth ranking of organization type %s is %s\n' \
                % (organization_type, revenue_growth_ranking)
+
+@route('/status', method = 'GET')
+def get_status():
+    return "It is working!\n"
 
 run(host='0.0.0.0', port=80)
