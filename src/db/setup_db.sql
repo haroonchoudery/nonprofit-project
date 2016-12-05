0704f02bd568d93739a9a2e2c6540988d233896e
Jerry Song <songlingyi@gmail.com>
2016-12-05 05:40:05 +0000

    modify the db schema

--- src/db/setup_db.sql
+++ src/db/setup_db.sql
@@ -5,12 +5,14 @@ USE organizations;
 # Create the primary table to store the data of tax-exempt organizations
 CREATE TABLE IF NOT EXISTS tax_exempt_organizations (
   electronic_id INT NOT NULL,
-  tax_year INT NOT NULL,
-  form_type VARCHAR(10) NOT NULL,
   organization_name TEXT,
   organization_type TEXT,
-  current_year_revenue INT,
-  prior_year_revenue INT,
-  annual_revenue_growth FLOAT(4,2),
-  PRIMARY KEY (electronic_id, tax_year)
+  cy_total_revenue INT,
+  py_total_revenue INT,
+  cy_contribution INT,
+  py_contribution INT,
+  cy_service_revenue INT,
+  py_service_revenue INT,
+  annual_totoal_revenue_growth FLOAT(4,2),
+  PRIMARY KEY (electronic_id)
 );
