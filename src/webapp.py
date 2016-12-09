d6cd5cefe0acccf1810ef7837e119a6c876bb01c
Haroon Choudery <haroon.choudery@gmail.com>
2016-12-09 15:21:22 -0800

    test

--- src/webapp.py
+++ src/webapp.py
@@ -14,12 +14,12 @@ def index():
 def get_revenue_growth():
     key = request.form['key']
     revenue_growth = mysql_client.query_revenue_growth(key)
-    # if revenue_growth is None:
-    #     return 'There is no organization with electronic id or name %s\n' % key
-    # else:
-    #     return 'The annual growth of the organization with electronic id or name %s is %s\n' % (key, revenue_growth)
-    print revenue_growth
-    return render_template("index.html", revenue_growth = revenue_growth)
+    if revenue_growth is None:
+        return 'There is no organization with electronic id or name %s\n' % key
+    else:
+        return 'The annual growth of the organization with electronic id or name %s is %s\n' % (key, revenue_growth)
+
+    # return render_template("index.html", revenue_growth = revenue_growth)
 
 @app.route('/ranking/<organization_type>/<limit>', methods = ['GET'])
 def get_revenue_growth_ranking(organization_type, limit):
