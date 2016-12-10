5e2d7a0f6bc634af03914ed223f1ccdb5618e1a3
Haroon Choudery <haroon.choudery@gmail.com>
2016-12-10 00:11:08 -0800

    test

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -21,3 +21,13 @@ $(function() {
         });
     });
 });
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
