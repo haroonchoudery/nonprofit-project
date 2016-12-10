ec5bc0e232ed8f16edf6c4a1687f594ff623e939
Haroon Choudery <haroon.choudery@gmail.com>
2016-12-09 20:14:35 -0800

    test

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -1,11 +1,12 @@
 $(function() {
 	var query = $('#query');
+	var key = $('#key');
 
     $(query).submit(function(event) {
     	event.preventDefault();
         $.ajax({
             url: '/score',
-            data: $('input[name="key"]').val().serialize(),
+            data: $(key).serialize(),
             type: 'POST',
             success: function(response) {
                 console.log(response);
