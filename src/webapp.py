d9489666a6404f33f6e6e0ee068d483d3fae8521
Haroon Choudery <haroon.choudery@gmail.com>
2016-12-09 19:33:50 -0800

    test

--- src/webapp.py
+++ src/webapp.py
@@ -25,7 +25,7 @@ def get_revenue_growth():
 
     key = request.form['key']
     revenue_growth = mysql_client.query_revenue_growth(key)
-    return json.dumps({'status':'OK','revenue_growth':revenue_growth});
+    return json.dumps({'status':'OK', 'key':key, 'revenue_growth':revenue_growth});
 
 @app.route('/ranking/<organization_type>/<limit>', methods = ['GET'])
 def get_revenue_growth_ranking(organization_type, limit):
