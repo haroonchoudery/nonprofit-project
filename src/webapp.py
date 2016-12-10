4edaf169962a90eb6446eeca4adef2813f8913dc
Haroon Choudery <haroon.choudery@gmail.com>
2016-12-09 17:29:26 -0800

    test

--- src/webapp.py
+++ src/webapp.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/score', methods = ['POST'])
+@app.route('/score', methods = ['GET'])
 def get_revenue_growth():
     # key = request.form['key']
     # revenue_growth = mysql_client.query_revenue_growth(key)
