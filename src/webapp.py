67c58f92f8f5dbb29c06bc8c7b76df5320ece17c
Haroon Choudery <haroon.choudery@gmail.com>
2016-12-09 17:07:51 -0800

    test

--- src/webapp.py
+++ src/webapp.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/score', methods = ['POST'])
 def get_revenue_growth():
-    key = request.form['key']
+    key = request.args.get('key')
     revenue_growth = mysql_client.query_revenue_growth(key)
     # if revenue_growth is None:
     #     return 'There is no organization with electronic id or name %s\n' % key
