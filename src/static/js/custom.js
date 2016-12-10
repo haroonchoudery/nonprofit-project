2047ab2fb83ce50ec218a734036684fb1af7f431
Haroon Choudery <haroon.choudery@gmail.com>
2016-12-09 18:13:40 -0800

    test

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -1,7 +1,7 @@
 $(function() {
     $('form#query').submit(function() {
       $.getJSON('/score', {key: $('input[name="key"]').val()}, function(data){
-        console.log(data);
+        $("#score").text(data.revenue_growth);
       });
       return false;
     });
