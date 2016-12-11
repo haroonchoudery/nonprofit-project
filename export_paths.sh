02d6b29e05671d5e6c4f1d709b5d1c9b3db9ada3
jerrysong <songlingyi@gmail.com>
2016-12-11 15:39:28 -0800

    update export_paths.sh

--- export_paths.sh
+++ export_paths.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # Export the pythonpath. Please source this file before starting the application.
 export PYTHONPATH=$PYTHONPATH:$(pwd)/src
+export CONFROOT=$(pwd)/etc
 echo $PYTHONPATH
+echo $CONFROOT
